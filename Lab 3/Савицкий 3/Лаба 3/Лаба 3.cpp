6edb9be089546ba6a333742b2ff43aadc25e0fe1
Denis <denik.savic2007@gmail.com>
2025-02-17 12:37:19 +0300

    finished lab3

---
+++ Lab 3/Савицкий 3/Лаба 3/Лаба 3.cpp
@@ -0,0 +1,332 @@
+﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>
+using namespace std;
+
+bool isvowel(char ch) {
+	if (ch == ' ') return true;
+	char vowels[] = "aeiou";
+	for (int i = 0; vowels[i] != '\0'; i++) {
+		if (ch == vowels[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int var;
+	cout << "Введите номер варианта: ";
+	cin >> var;
+	switch (var) {
+	case 10: {
+		int part;
+		cout << "Введите номер задания: ";
+		cin >> part;
+		switch (part) {
+		case 1: {
+			fstream fin("FILE1.txt");
+			fstream fout("FILE2.txt");
+			char word[50];
+			fin >> word;
+			fin.seekg(0);
+			while (!fin.fail()) {
+				char str[100];
+				fin.getline(str, 100);
+				int i = 0;
+				int j = 0;
+				bool flag = false;
+				while (str[i - 1] != '\0') {
+					if (str[i] == ' ' || str[i] == '\0') {
+						if (word[j] == '\0') {
+							flag = true;
+							break;
+						}
+					}
+					if (word[j] != str[i]) {
+						j = 0;
+						while (str[i - 1] != '\0' && str[i - 1] != ' ') i++;
+					}
+					i++;
+					j++;
+				}
+				if (flag) {
+					fout << str << '\n';
+				}
+			}
+			char str[100];
+			fout.seekg(0);
+			fout.getline(str, 100);
+			int sum = 0;
+			for (int i = 0; str[i] != '\0'; i++) {
+				if (!isvowel(str[i])) {
+					sum++;
+				}
+			}
+			cout << "Количество согласных:" << sum;
+			fout.close();
+			fin.close();
+			break;
+		}
+		case 2:
+			ofstream fout("FILE10_2.txt");
+			char str[256];
+			cin.ignore(cin.rdbuf()->in_avail());
+			cin.getline(str, 256);
+			fout << str;
+			fout.close();
+			ifstream fin("FILE10_2.txt");
+			char buf[50];
+			while (fin >> buf) {
+				int i = 0;
+				while (buf[i] != '\0') {
+					if (buf[i] == 'p') {
+						cout << buf << ' ';
+						break;
+					}
+					i++;
+				}
+			}
+			fin.close();
+		}
+		break;
+	}
+	case 11: {
+		int part;
+		cout << "Введите номер задания: ";
+		cin >> part;
+		switch (part) {
+		case 1: {
+			ifstream fin("FILE11_1.txt");
+			ofstream fout("FILE11_2.txt");
+			char buf[256];
+			while (!fin.fail()) {
+				fin.getline(buf, 256);
+				int i = 0;
+				bool flag = true;
+				while (buf[i] != '\0') {
+					if (buf[i] == ' ') {
+						if (buf[i + 1] != '\0' && buf[i + 1] != ' ') {
+							flag = false;
+							break;
+						}
+					}
+					i++;
+				}
+				if (flag) {
+					fout << buf << '\n';
+				}
+			}
+			fin.close();
+			fout.close();
+			fin.open("FILE11_2.txt");
+			int sum = 0;
+			while (!fin.fail()) {
+				char ch = fin.get();
+				sum++;
+			}
+			cout << "Количество символов в файле: " << sum;
+			break;
+		}
+		case 2: {
+			ofstream fout("FILE11_3.txt");
+			char buf[256];
+			cout << "Первая строка:\n";
+			cin.ignore(cin.rdbuf()->in_avail());
+			cin.getline(buf, 256);
+			fout << buf << '\n';
+			cout << "Вторая строка:\n";
+			cin.ignore(cin.rdbuf()->in_avail());
+			cin.getline(buf, 256);
+			fout << buf;
+			fout.close();
+			ifstream fin("FILE11_3.txt");
+			fin.getline(buf, 256);
+			int min = 256, i = 0, start = 0;
+			while (buf[i - 1] != '\0') {
+				if (buf[i] == ' ' || buf[i] == '\0') {
+					if (i - start < min) {
+						min = i - start;
+					}
+					start = i + 1;
+				}
+				i++;
+			}
+			cout << "Минимальная длина слова: " << min;
+			int max = 0;
+			i = 0;
+			start = 0;
+			while (buf[i - 1] != '\0') {
+				if (buf[i] == ' ' || buf[i] == '\0') {
+					if (i - start > max) {
+						max = i - start;
+					}
+					start = i + 1;
+				}
+				i++;
+			}
+			cout << "\nМаксимальная длина слова: " << max;
+			break;
+		}
+		}
+		break;
+	}
+	case 12: {
+		int part;
+		cout << "Введите номер задания: ";
+		cin >> part;
+		switch (part) {
+		case 1: {
+			fstream fin("FILE12_1.txt");
+			fstream fout("FILE12_2.txt");
+			char word[100];
+			fin >> word;
+			fin.seekg(0);
+			while (!fin.fail()) {
+				char str[256];
+				fin.getline(str, 100);
+				int i = 0;
+				int j = 0;
+				bool flag = false;
+				while (str[i - 1] != '\0') {
+					if (str[i] == ' ' || str[i] == '\0') {
+						if (word[j] == '\0') {
+							flag = true;
+							break;
+						}
+					}
+					if (word[j] != str[i]) {
+						j = 0;
+						while (str[i - 1] != '\0' && str[i - 1] != ' ') i++;
+					}
+					i++;
+					j++;
+				}
+				if (flag) {
+					fout << str << '\n';
+				}
+			}
+			fout << '\0';
+			fout.close();
+			fin.close();
+			fin.open("FILE12_2.txt");
+			int sum = 0;
+			char ch = fin.get();
+			while (ch != '\0') {
+				if (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z')
+					sum++;
+				ch = fin.get();
+			}
+			cout << "Количество букв: " << sum;
+			fin.close();
+			break;
+		}
+		case 2: {
+			ofstream fout("FILE12_3.txt");
+			char buf[256];
+			cin.ignore(cin.rdbuf()->in_avail());
+			cin.getline(buf, 256);
+			fout << buf;
+			fout.close();
+			ifstream fin("FILE12_3.txt");
+			while (fin>>buf) {
+				int i = 0;
+				bool flag = true;
+				while (buf[i] != '\0') {
+					if (!(buf[i] >= '0' && buf[i] <= '9')) {
+						flag = false;
+						break;
+					}
+					i++;
+				}
+				if (flag) {
+					if ((buf[i - 1] - '0') % 2 == 1) {
+						cout << buf << ' ';
+					}
+				}
+			}
+			fin.close();
+			break;
+		}
+		}
+		break;
+	}
+	case 13: {
+		int var;
+		cout << "Введите номер задания: ";
+		cin >> var;
+		switch (var) {
+		case 1: {
+			ifstream fin("FILE13_1.txt");
+			ofstream fout("FILE13_2.txt");
+			char buf[256];
+			while (!fin.fail()) {
+				fin.getline(buf, 256);
+				int k = 0, i = 0;
+				while (buf[i] != '\0') {
+					if (buf[i] == ' ') {
+						if (!(buf[i + 1] == ' ' || buf[i + 1] == '\0')) {
+							k++;
+						}
+					}
+					i++;
+				}
+				if (k > 1) {
+					fout << buf << '\n';
+				}
+			}
+			fin.close();
+			fout.close();
+			fin.open("FILE13_2.txt");
+			int max = -1, j = 1, maxj = 0;
+			while (fin>>buf) {
+				int i = 0;
+				int sum = 0;
+				while (buf[i] != '\0') {
+					if (isvowel(buf[i])) {
+						sum++;
+					}
+					i++;
+				}
+				if (sum > max) {
+					max = sum;
+					maxj = j;
+				}
+				j++;
+			}
+			cout << "Максимальное количество глассных: " << max << " в слове под номером: " << maxj;
+			break;
+		}
+		case 2: {
+			ofstream fout("FILE13_3.txt");
+			char buf[256];
+			cin.ignore(cin.rdbuf()->in_avail());
+			cin.getline(buf, 256);
+			fout << buf<<'\0';
+			fout.close();
+			ifstream fin("FILE13_3.txt");
+			char ch = fin.get();
+			char skobki[6] = { '(',')','[',']','{','}' };
+			int sum[6] = { 0,0,0,0,0,0 };
+			while (ch != '\0') {
+				for (int i = 0; i < 6; i++) {
+					if (ch == skobki[i]) {
+						sum[i]++;
+					}
+				}
+				ch = fin.get();
+			}
+			for (int i = 0; i < 6; i++) {
+				cout << "Количество скобок вида: " << skobki[i] << " - " << sum[i]<<'\n';
+			}
+			break;
+		}
+		}
+		break;
+	}
+	}
+	return 0;
+}
---
+++ Lab 4/Савицкий 4/Савицкий 4.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+struct Cars
+{
+	char brand[20];
+	char color[20];
+	int serial;
+	char release[20];
+	char type[20];
+	char check[20];
+	char owner[20];
+};
+void readMas(Cars all[20],int &size) {
+	cin >> all[size].brand >> all[size].color >> all[size].serial >> all[size].release >> all[size].type >> all[size].check >> all[size].owner;
+	size++;
+	return;
+}
+void writeMas(Cars all[20], int size) {
+	cout << "Марка автомобиля цвет\t заводской номер\t дата выпуска\t тип кузова\tдата последнего техосмотра\tвладелец";
+	for (int i = 0; i < size; i++) {
+		cout << all[size].brand << "\t" << all[size].color << "\t" << all[size].serial << "\t" << all[size].release << "\t" << all[size].type << "\t" << all[size].check << "\t" << all[size].owner;
+		cout << "\n";
+	}
+	return;
+}
+void writeFile(Cars all[20], int size, ofstream fout) {
+	fout.clear();
+	for (int i = 0; i < size; i++) {
+		fout << all[size].brand << "\t" << all[size].color << "\t" << all[size].serial << "\t" << all[size].release << "\t" << all[size].type << "\t" << all[size].check << "\t" << all[size].owner;
+		fout << "\n";
+	}
+	return;
+}
+void readFile(Cars all[20], int size, ifstream fin) {
+	Cars newall[20];
+	fin.seekg(0);
+	while (fin >> newall[size].brand) {
+		fin >> newall[size].color >> newall[size].serial >> newall[size].release >> newall[size].type >> newall[size].check >> newall[size].owner;
+		size++;
+	}
+	all = newall;
+	return;
+}
+void searchMas(Cars all[20], int size, Cars item) {
+	for (int i = 0; i < size; i++) {
+	}
+}
+int main() {
+	setlocale(LC_ALL, "rus");
+	Cars all[20];
+	int size = 0;
+	ifstream fin("Var10.txt");
+	ofstream fout("Var10.txt");
+	while (fin >> all[size].brand) {
+		fin >> all[size].color >> all[size].serial >> all[size].release >> all[size].type >> all[size].check >> all[size].owner;
+		size++;
+	}
+	fin.close();
+	int choice;
+	cin >> choice;
+	do {
+
+		cout << "\n1.Ввод данных с клавиатуры и запись в массив\n";
+		cout << "2.Вывод данных из массива\n";
+		cout << "3.Перенос данных из массива в файл\n";
+		cout << "4.Вывод данных из файла\n";
+		cout << "5.Поиск по владельцу\n";
+		cout << "6.Удаление данных из массива\n";
+		cout << "0.Выход из программы\n\n";
+		cout << "Введите номер операции: ";
+		cin >> choice;
+		switch (choice) {
+		case 1: {
+
+		}
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 0: {
+			break;
+		}
+		default: {
+			cout << "Неверный ввод\n";
+			break;
+		}
+		}
+	} while (choice != 0);
+	return 0;
+}
